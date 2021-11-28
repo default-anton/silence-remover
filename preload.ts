841e8a456f84fa98acdd39a8a592bb774a74171d
Anton Kuzmenko <1917237+NoNonsenseDev@users.noreply.github.com>
2021-11-28 00:46:29 -0800

    Second attempt to append two clips of the same video to a timeline

--- preload.ts
+++ preload.ts
@@ -16,71 +16,73 @@ if (
 }
 
 class Clip {
-  mediaPoolItem: any;
-  name: string;
-  clipType: string;
-  filePath: string;
-  fps: number;
-  duration: string;
-  sampleRate: string;
-  syncedAudio?: string;
-  start?: string;
-  end?: string;
+  readonly mediaPoolItem: any;
 
-  constructor(
-    mediaPoolItem: any,
-    name: string,
-    clipType: string,
-    filePath: string,
-    fps: number,
-    duration: string,
-    sampleRate: string,
-    syncedAudio?: string,
-    start?: string,
-    end?: string
-  ) {
+  constructor(mediaPoolItem: any) {
     this.mediaPoolItem = mediaPoolItem;
-    this.name = name;
-    this.clipType = clipType;
-    this.filePath = filePath;
-    this.fps = fps;
-    this.duration = duration;
-    this.sampleRate = sampleRate;
-    this.syncedAudio = syncedAudio;
-    this.start = start;
-    this.end = end;
+  }
+
+  set startFrame(startFrame: number) {
+    this.mediaPoolItem.SetClipProperty(
+      "In",
+      this.framesToDavinciTS(startFrame)
+    );
+  }
+
+  set endFrame(endFrame: number) {
+    this.mediaPoolItem.SetClipProperty("Out", this.framesToDavinciTS(endFrame));
+  }
+
+  get name(): string {
+    return this.mediaPoolItem.GetName();
+  }
+
+  get clipType(): string {
+    return this.mediaPoolItem.GetClipProperty("Type");
+  }
+
+  get filePath(): string {
+    return this.mediaPoolItem.GetClipProperty("File Path");
+  }
+
+  get fps(): number {
+    return parseInt(this.mediaPoolItem.GetClipProperty("FPS"));
+  }
+
+  get duration(): string {
+    return this.mediaPoolItem.GetClipProperty("Duration");
+  }
+
+  get sampleRate(): string {
+    return this.mediaPoolItem.GetClipProperty("Sample Rate");
+  }
+
+  get syncedAudio(): string | undefined {
+    return this.mediaPoolItem.GetClipProperty("Synced Audio");
   }
 
   isVideo(): boolean {
-    return this.clipType === "Video + Audio";
-  }
-
-  get clipInfo(): ClipInfo {
-    return {
-      mediaPoolItem: this.mediaPoolItem,
-      startFrame: this.startFrame,
-      endFrame: this.endFrame,
-    };
+    return this.clipType === "Video + Audio" || this.clipType === "Video";
   }
 
   get startFrame(): number {
-    if (this.start === null) {
+    const start: string | undefined = this.mediaPoolItem.GetClipProperty("In");
+
+    if (start === null) {
       return 0;
     }
 
-    return this.davinciTSToFrames(this.start);
+    return this.davinciTSToFrames(start);
   }
 
   get endFrame(): number {
-    if (this.end === null) {
-      return this.frames;
+    const end: string | undefined = this.mediaPoolItem.GetClipProperty("Out");
+
+    if (end === null) {
+      return this.davinciTSToFrames(this.duration);
     }
 
-    return this.davinciTSToFrames(this.end);
-  }
-
-  get frames(): number {
-    return this.davinciTSToFrames(this.duration);
+    return this.davinciTSToFrames(end);
   }
 
   private davinciTSToFrames(ts: string): number {
@@ -92,13 +94,32 @@ class Clip {
       this.fps * 3600 * parseInt(hours)
     );
   }
-}
 
-interface ClipInfo {
-  mediaPoolItem: any;
-  startFrame: number;
-  endFrame: number;
-  mediaType?: 1 | 2; // (int; 1 - Video only, 2 - Audio only)
+  private framesToDavinciTS(frames: number): string {
+    const hours = Math.floor(frames / (this.fps * 3600));
+    const minutes = Math.floor(
+      (frames - hours * this.fps * 3600) / (this.fps * 60)
+    );
+    const seconds = Math.floor(
+      (frames - hours * this.fps * 3600 - minutes * this.fps * 60) / this.fps
+    );
+    const framesStr =
+      frames -
+      hours * this.fps * 3600 -
+      minutes * this.fps * 60 -
+      seconds * this.fps;
+
+    return `${this.leftPad(hours, 2)}:${this.leftPad(
+      minutes,
+      2
+    )}:${this.leftPad(seconds, 2)}:${framesStr}`;
+  }
+
+  private leftPad(num: number, size: number): string {
+    let s: string = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -115,45 +136,39 @@ window.addEventListener("DOMContentLoaded", () => {
   const clipsInCurrentFolder = currentFolder.GetClipList();
   const clips: Clip[] = [];
   for (const davinciClip of clipsInCurrentFolder) {
-    const clip = new Clip(
-      davinciClip,
-      davinciClip.GetName(),
-      davinciClip.GetClipProperty("Type"),
-      davinciClip.GetClipProperty("File Path"),
-      davinciClip.GetClipProperty("FPS"),
-      davinciClip.GetClipProperty("Duration"),
-      davinciClip.GetClipProperty("Sample Rate"),
-      davinciClip.GetClipProperty("Synced Audio"),
-      davinciClip.GetClipProperty("In"),
-      davinciClip.GetClipProperty("Out")
-    );
+    const clip = new Clip(davinciClip);
     if (!clip.isVideo()) {
       continue;
     }
     clips.push(clip);
   }
-  const timelineClips: ClipInfo[] = [];
-  for (const clip of clips) {
-    if (clip.isVideo()) {
-      const info = clip.clipInfo;
-      if (info.endFrame - info.startFrame < 3) {
-        timelineClips.push(info);
-        continue;
-      }
-      const info2 = { ...info };
-      info.endFrame = Math.floor(info.endFrame / 2);
-      info2.startFrame = info.endFrame + clip.fps + 1;
-      timelineClips.push(info);
-      timelineClips.push(info2);
-    }
-  }
-  console.log("timelineClips", timelineClips);
 
   const timeline = mediaPool.CreateEmptyTimeline("TestTimeline");
   if (!timeline) {
     console.error("Error: Failed to create timeline!");
   }
-  for (const clip of timelineClips) {
-    mediaPool.AppendToTimeline(clip);
+
+  for (const clip of clips) {
+    if (clip.isVideo()) {
+      if (clip.endFrame - clip.startFrame < 3) {
+        mediaPool.AppendToTimeline(clip.mediaPoolItem);
+        continue;
+      }
+
+      const originalStartFrame = clip.startFrame;
+      const originalEndFrame = clip.endFrame;
+      const clip1EndFrame = Math.floor(originalEndFrame / 2);
+
+      clip.endFrame = clip1EndFrame;
+      mediaPool.AppendToTimeline(clip.mediaPoolItem);
+
+      clip.endFrame = originalEndFrame;
+      clip.startFrame = clip1EndFrame + 1;
+      mediaPool.AppendToTimeline(clip);
+
+      // revert the clip to its original state
+      clip.startFrame = originalStartFrame;
+      clip.endFrame = originalEndFrame;
+    }
   }
 });
